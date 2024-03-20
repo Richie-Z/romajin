cc6b3fc5f28686c8128b1fcd14cbd91bdc78c053
Richie-Z <zakariarichie@yahoo.com>
2024-03-20 14:32:45 +0700

    feat: add setting modal view

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,24 @@
+import React from 'react';
 import './styles/lyric-text.scss'
 import { translatorService } from "@services/translator.service";
 import { waitForSpicetify } from "@/utils";
+import SettingsModal from '@components/SettingsModal';
 
 async function main(): Promise<void> {
   await waitForSpicetify()
   await translatorService.init()
+
+  new Spicetify.Menu.Item(
+    'Romajin Settings',
+    false,
+    (self) => {
+      Spicetify.PopupModal.display({
+        title: 'Romajin Settings',
+        content: React.createElement(SettingsModal) as any,
+        isLarge: true
+      })
+    },
+  ).register()
 }
 
 export default main;
---
+++ src/components/SettingsModal.tsx
@@ -0,0 +1,55 @@
+import '../styles/romajin-settings.scss'
+import React, { useState } from 'react'
+import SwitchButton from './SwitchButton'
+import { supportedLanguages } from '@translate-tools/core/translators/GoogleTranslator'
+
+export default function SettingModal() {
+  const [translate, setTranslate] = useState({
+    active: true,
+    targetLanguage: 'en',
+    corsProxy: ''
+  })
+  return (
+    <div className='romajin-settings'>
+      <div className='setting-box'>
+        <p className='title'>Translate</p>
+        <div className={`card ${translate ? 'active' : null}`}>
+          <div className='input'>
+            <div className="title">Enabled</div>
+            <SwitchButton
+              onChange={(e) => setTranslate((data) => ({ ...data, active: e as boolean }))}
+              defaultChecked={translate.active}
+            />
+          </div>
+          <p className="description">Enable the google translator</p>
+        </div>
+        {translate && (
+          <div className="translate-child">
+            <div className="card child">
+              <div className='input'>
+                <div className="title">Target Language</div>
+                <select className="input-field" onChange={(e) => setTranslate((data) => ({ ...data, targetLanguage: e.target.value }))}>
+                  {supportedLanguages.map((lang) =>
+                    <option
+                      value={lang}
+                      key={lang}
+                      selected={lang === translate.targetLanguage}>{lang}</option>
+                  )}
+                </select>
+              </div>
+            </div>
+            <div className="card child">
+              <div className='input'>
+                <div className="title">Cors Proxy</div>
+                <input type="text" className='input-field'
+                  onChange={(e) => setTranslate((data) => ({ ...data, corsProxy: e.target.value }))}
+                  defaultValue={translate.corsProxy}
+                />
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div >
+  )
+}
