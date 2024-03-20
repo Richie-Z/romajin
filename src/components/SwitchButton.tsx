bdb3667cb42af7b884285ac42f596d68dccb75b3
Richie-Z <zakariarichie@yahoo.com>
2024-03-20 14:31:09 +0700

    feat: introduce switch buttno

---
+++ src/components/SwitchButton.tsx
@@ -0,0 +1,11 @@
+import '../styles/switch-button.scss'
+import React, { HtmlHTMLAttributes } from "react";
+
+export default function SwitchButton({ onChange, ...props }: { onChange: (val: boolean) => void } & HtmlHTMLAttributes<HTMLInputElement>) {
+  return (
+    <label className="switch">
+      <input type="checkbox" onChange={(e) => onChange(e.target.checked)} {...props} />
+      <span className="slider round"></span>
+    </label>
+  )
+}
