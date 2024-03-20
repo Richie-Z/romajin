9bc21f11272950aa8bce0e0c08d2e53662a166ee
Richie-Z <zakariarichie@yahoo.com>
2024-03-20 09:25:06 +0700

    refactor: move the translator method to service

--- src/app.tsx
+++ src/app.tsx
@@ -1,49 +1,20 @@
 import './styles/lyric-text.scss'
 import { kuroshiroService } from "@services/kuroshiro.service";
-import { waitForSpicetify, isJapanese, getElement, createElement, translateJapaneseToEnglish, checkDomExists } from "@utils/index";
+import { translatorService } from "@services/translator.service";
+import { waitForSpicetify, checkDomExists } from "@utils/index";
 
 async function main(): Promise<void> {
   await waitForSpicetify()
   await kuroshiroService.init()
   let isAlreadyTranslated = false;
 
-  const convertLyric: () => Promise<[string, string | null]> = async () => {
-    let originalLyric = "";
-    (getElement({ selector: ".lyrics-lyricsContent-lyric", isAll: true }) as NodeListOf<HTMLElement>).
-      forEach((val) => originalLyric += `${val.textContent}\n`)
-    let romajiLyric: string | null = null;
-    if (isJapanese(originalLyric)) {
-      romajiLyric = await kuroshiroService.convert(originalLyric);
-    }
-    isAlreadyTranslated = true;
-    return [originalLyric, romajiLyric]
-  }
-
-  const renderLyric = (originalLyric: string, romajiLyric: string) => {
-    const lyricsBox = getElement({ selector: '.lyrics-lyricsContent-lyric', isAll: true }) as NodeListOf<Element>;
-    lyricsBox.forEach((val, i) => {
-      const oriLyric = originalLyric.split('\n')[i]
-      const oriLyricBox = createElement({ className: 'lyrics-lyricsContent-text' }); oriLyricBox.textContent = oriLyric
-      const subLyric = romajiLyric.split('\n')[i];
-      (async () => {
-        if (!isJapanese(oriLyric)) return
-        const translated = await translateJapaneseToEnglish(oriLyric)
-        let englishLyricBox = createElement({ className: 'lyrics-lyricsContent-text sub english' }); englishLyricBox.textContent = translated
-        val.insertBefore(englishLyricBox, oriLyricBox)
-      })()
-      if (subLyric === " ♪ ") return;
-      const subLyricBox = createElement({ className: 'lyrics-lyricsContent-text sub' }); subLyricBox.textContent = subLyric
-      val.replaceChildren(oriLyricBox, subLyricBox)
-    });
-  }
-
   setInterval(async () => {
     if (checkDomExists({ selector: "div.lyrics-lyrics-container" })) {
-      if (!isAlreadyTranslated) {
-        const [originalLyric, romajiLyric] = await convertLyric()
-        if (!romajiLyric) return
-        renderLyric(originalLyric, romajiLyric)
-      }
+      if (isAlreadyTranslated) return
+      translatorService.convertLyric().then(() => {
+        translatorService.renderLyric()
+        isAlreadyTranslated = true;
+      })
     } else {
       isAlreadyTranslated = false;
     }
---
+++ src/services/translator.service.ts
@@ -0,0 +1,46 @@
+import { createElement, getElement, isJapanese, translateJapaneseToEnglish } from "@/utils";
+import { kuroshiroService } from "./kuroshiro.service";
+
+export class TranslatorService {
+  private originalLyric: string = "";
+  private romajiLyric: string | null = null;
+
+  public async convertLyric(): Promise<[string, string | null]> {
+    let originalLyric = "";
+    (getElement({ selector: ".lyrics-lyricsContent-lyric", isAll: true }) as NodeListOf<HTMLElement>).
+      forEach((val) => originalLyric += `${val.textContent}\n`)
+    let romajiLyric: string | null = null;
+    if (isJapanese(originalLyric)) {
+      romajiLyric = await kuroshiroService.convert(originalLyric);
+    }
+
+    this.originalLyric = originalLyric
+    this.romajiLyric = romajiLyric
+
+    return [originalLyric, romajiLyric]
+  }
+
+  public renderLyric(): void {
+    if (this.romajiLyric === null) return
+    const lyricsBox = getElement({ selector: '.lyrics-lyricsContent-lyric', isAll: true }) as NodeListOf<Element>;
+
+    lyricsBox.forEach((val, i) => {
+      const oriLyric = this.originalLyric.split('\n')[i]
+      const oriLyricBox = createElement({ className: 'lyrics-lyricsContent-text' }); oriLyricBox.textContent = oriLyric;
+
+      (async () => {
+        if (oriLyric === " ♪ " || oriLyric.length === 0 || !isJapanese(oriLyric)) return
+        const translated = await translateJapaneseToEnglish(oriLyric)
+        let englishLyricBox = createElement({ className: 'lyrics-lyricsContent-text sub english' }); englishLyricBox.textContent = translated
+        val.insertBefore(englishLyricBox, oriLyricBox)
+      })()
+
+      const subLyric = this.romajiLyric!.split('\n')[i]
+      if (subLyric === " ♪ ") return;
+      const subLyricBox = createElement({ className: 'lyrics-lyricsContent-text sub' }); subLyricBox.textContent = subLyric
+      val.replaceChildren(oriLyricBox, subLyricBox)
+    });
+  }
+}
+
+export const translatorService = new TranslatorService()
